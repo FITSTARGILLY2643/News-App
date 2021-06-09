a201dde29a51493e7e69dd3d1d8170c7249cc708
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-10 00:47:12 +0300

    Project complete

--- config.py
+++ config.py
@@ -30,4 +30,5 @@ class DevConfig(Config):
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
+
 }
--- start.sh
+++ start.sh
@@ -2,3 +2,8 @@ export NEWS_API_KEY='92024ca524b9462782a214ac25c41263'
 export SECRET_KEY='34265182'
 
 python3.8 manage.py server
+
+# export NEWS_API_KEY='92024ca524b9462782a214ac25c41263'
+# export SECRET_KEY='34265182'
+
+# python3.8 manage.py server
